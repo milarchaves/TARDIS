7c340b6ed78f8823ffba6aceec705f9c3e19e8f2
camila <camilachaves@biof.ufrj.br>
2022-04-18 20:38:38 -0300

    Add homology-search function

---
+++ homology-search.py
@@ -0,0 +1,24 @@
+import sys
+import os
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
+
+for out_dir in os.listdir(os.getcwd()):
+    if os.path.isdir(out_dir):
+        os.getcwd(out_dir)
+        try:
+            sequence_data = open(out_dir + '_CHOIR_MonomerSequence.fasta').read()
+            result_handle = NCBIWWW.qblast("blastp", "nr", sequence_data, entrez_query='txid9606[ORGN]')
+            with open('results.xml', 'a') as save_file:
+               blast_results = result_handle.read()
+               save_file.write(blast_results)
+
+            E_VALUE_THRESH = 1e-20
+            for record in NCBIXML.parse(open("results.xml")):
+                if record.alignments:
+                    print("\n")
+                    print("query: %s" % record.query[:100])
+                    for align in record.alignments:
+                       for hsp in align.hsps:
+                          if hsp.expect < E_VALUE_THRESH:
+                             print("match: %s " % align.title[:100])
