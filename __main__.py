32fe6d215e977867601de35404f5b64bca1624dd
Camila <camilachaves@biof.ufrj.br>
2023-01-17 09:39:00 -0300

    code organization

---
+++ __main__.py
@@ -0,0 +1,41 @@
+#!/usr/lib/python3
+
+# Imports
+###############################################################################
+from TARDIS.Initialise import *
+
+# License
+###############################################################################
+'''
+
+TARDIS: TARgets DIScoverer
+
+Authors: Chaves, C; Torres, P.H.M.
+
+[Federal University of Rio de Janeiro]
+
+Contact info:
+E-mail address: chaves.camila13@gmail.com
+Github: https://github.com/milarchaves
+This project is licensed under Creative Commons license (CC-BY-4.0)
+
+'''
+
+# Description
+###############################################################################
+
+# Classes
+###############################################################################
+
+# Functions
+###############################################################################
+
+# Main Function
+###############################################################################
+def main():
+    pass
+
+# Execute
+###############################################################################
+if __name__ == "__main__":
+    main()
