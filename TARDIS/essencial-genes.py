fb1516205d00f4ebe0b5f4d5bedce77161a798aa
Camila <camilachaves@biof.ufrj.br>
2023-01-26 15:11:18 -0300

    Added chokepoint summary spreadsheet

--- TARDIS/essencial-genes.py
+++ TARDIS/essencial-genes.py
@@ -3,19 +3,55 @@
 '''
 import pandas as pd
 import cobra
+from findCPcore import CobraMetabolicModel
 from cobra.flux_analysis import ( single_gene_deletion, single_reaction_deletion, double_gene_deletion, double_reaction_deletion)
 from cobra.flux_analysis import flux_variability_analysis
 
 '''
 ++++++ File path ++++++
 '''
-model = '/home/camila/LMDM/P.Aeruginosa/CCBH4851/CCBH4851-metabolic-network/CCBH4851_metabolic_map_v2.xml'
+model = '/home/camila/LMDM/Mestrado/TARDIS/test/d_radiodurans.xml'
 
 #open sbml metabolic model
 model = cobra.io.read_sbml_model(model)
 
+model = CobraMetabolicModel('/home/camila/LMDM/Mestrado/TARDIS/test/d_radiodurans.xml')
+
 #maximize flux through the objective reactions
 model.optimize()
+
+# update flux bounds with FVA
+model.fva(update_flux=True)
+
+# compute chokepoints
+model.find_chokepoints()
+
+# get chokepoints
+model.chokepoints()
+
+model.find_essential_genes_1()
+
+model.essential_genes()
+
+
+#Generates chokepoint summary spreadsheet
+from findCPcore import Facade
+from findCPcore import FacadeUtils
+
+def callback_print_ignore(message, arg1, arg2):
+    pass
+
+def callback_print_logger(message, arg1, arg2):
+    logger = arg1
+    logger.info(message)
+
+def callback_print(message, arg1, arg2):
+    print(arg1 + message)
+
+facadeUtils = FacadeUtils()
+spreadsheet = facadeUtils.run_summary_model('/home/camila/LMDM/Mestrado/TARDIS/test/d_radiodurans.xml', callback_print, "LOG:", None, fraction=0.95)
+facadeUtils.save_spreadsheet("output.xls", spreadsheet)
+
 
 #finds the ranges of each metabolic flux at 90% optimality (literature review)
 model.summary(fva=0.9)
